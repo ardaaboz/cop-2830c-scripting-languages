11cff65a896533eeb76bb7bfe42cc2b71fb5f493
Arda Boz <ardaboz4317@gmail.com>
2025-05-19 13:23:08 +0300

    Implemented functions and lists

--- projects/conditional-logic-interpreter/main.py
+++ projects/conditional-logic-interpreter/main.py
@@ -1,42 +1,30 @@
 # Should I play DnD tonight?
 
+questions = [
+    "Do you feel like playing?",
+    "Is the session scheduled for today?",
+    "Do you have time?",
+    "Do you need to do homework?",  # Note: "yes" means you shouldn't play
+    "Are all your friends ready?",
+    "Is the session prepared?"
+]
+
+def userChoice(answer):
+    if answer.lower() == "y":
+        return True
+    else:
+        return False
+
+# List to store the answers
+conditions = []
+
 print("Answer these questions with y/n:")
 
-answer = input("Do you feel like playing? ")
-if answer == 'y':
-    feel_like_playing = True
-else:
-    feel_like_playing = False
+for question in questions:
+    answer = input(question + " ")
+    conditions.append(userChoice(answer))
 
-answer = input("Is the session scheduled for today? ")
-if answer == 'y':
-    session_today = True
-else:
-    session_today = False
-
-answer = input("Do you have time? ")
-if answer == 'y':
-    have_time = True
-else:
-    have_time = False
-
-answer = input("Do you need to do homework? ")
-if answer == 'y':
-    have_homework = True
-else:
-    have_homework = False
-
-answer = input("Are all your friends ready? ")
-if answer == 'y':
-    friends_ready = True
-else:
-    friends_ready = False
-
-answer = input("Is the session prepared? ")
-if answer == 'y':
-    session_prepared = True
-else:
-    session_prepared = False
+feel_like_playing, session_today, have_time, have_homework, friends_ready, session_prepared = conditions
 
 if (feel_like_playing or session_today) and (have_time and not have_homework) and (friends_ready and session_prepared):
     print("You should play DnD tonight!")
